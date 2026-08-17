f5938694353137bc6d2911708ef32a8aff89b70c
Jeffrey Bostoen <support@jeffreybostoen.be>
2026-08-17 16:21:50 +0200

    * Enhancement: Deprecate TraceLog #12

--- components/TraceLog.class.inc.php
+++ components/TraceLog.class.inc.php
@@ -12,39 +12,46 @@ namespace jb_itop_extensions\components;
 
 /**
  * Class TraceLog
+ *
+ * @deprecated Extensions should implement their own `Helper::Trace()` delegating to a `Logger` (extends `\LogAPI`),
+ * matching the pattern already used in other jbostoen extensions (e.g. itop-jb-tags). See GitHub issue #12.
  */
 abstract class TraceLog {
-	
+
 	/**
 	 * Prints a $sMessage in the CRON output.
+	 *
+	 * @deprecated See GitHub issue #12. Use an extension-specific `Helper::Trace()` delegating to `\LogAPI` instead.
 	 *
 	 * @param \String $sMessage Message to put in the trace log (CRON output)
 	 * @param \String $sType Type of message. Possible values: 'info' (default), 'error'
 	 * @param \String $sWantedTraceLevel Wanted trace level. Possible values: 'none', 'info' (default), 'error'
 	 */
 	public static function Trace($sMessage, $sType = 'info', $sWantedTraceLevel = 'info') {
-		
+
+		@trigger_error('TraceLog::Trace() is deprecated. Extensions should implement their own Helper::Trace() delegating to a Logger (extends \LogAPI) instead. See GitHub issue #12.', E_USER_DEPRECATED);
+
 		switch($sWantedTraceLevel) {
-			
+
 			case 'info':
-				if(in_array($sType, ['info', 'error']) == true) {
+				if(in_array($sType, ['info', 'error'])) {
 					echo $sMessage. PHP_EOL;
 				}
 				break;
-				
+
 			case 'error':
-				if($sType == 'error') {
+				if($sType === 'error') {
 					echo $sMessage. PHP_EOL;
 				}
 				break;
-			
+
 			case 'none':
 				break;
-				
+
 			default:
-				echo 'Unexpected trace level: '.$this->sDebugLevel. PHP_EOL;
-			
+				echo 'Unexpected trace level: '.$sWantedTraceLevel. PHP_EOL;
+
 		}
 	}
-	
+
 }
